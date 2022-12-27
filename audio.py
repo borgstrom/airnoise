8236c2233e64aaf49d5f04576a8d89a410e01b62
Borgstrom <evan@borgstrom.ca>
2022-12-26 21:20:15 -0800

    Checkpoint

--- audio.py
+++ audio.py
@@ -1,6 +1,10 @@
-from typing import Dict
+import audioop
+from typing import Dict, Iterator
 
 import pyaudio
+
+SAMPLING_RATE = 22050
+NUM_SAMPLES = 1024 * 4
 
 
 class Audio:
@@ -18,3 +22,16 @@ class Audio:
                 continue
             out[n] = info["name"]
         return out
+
+    def rms(self) -> Iterator[int]:
+        stream = self.audio.open(
+            format=pyaudio.paInt16,
+            channels=1,
+            rate=SAMPLING_RATE,
+            input=True,
+            frames_per_buffer=NUM_SAMPLES,
+        )
+        while True:
+            data = stream.read(NUM_SAMPLES)
+            rms = audioop.rms(data, 2)
+            yield rms
--- cli.py
+++ cli.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 import click
 
 from audio import Audio
+from mqtt import MQTT
 
 log = logging.getLogger()
 
@@ -12,18 +13,36 @@ log = logging.getLogger()
 class Context:
     debug: bool
     audio: Audio
+    mqtt: MQTT
 
 
 @click.group()
 @click.option("--debug/--no-debug", default=False)
+@click.option("--mqtt-host", required=True)
+@click.option("--mqtt-username", required=True)
+@click.option("--mqtt-password", required=True)
 @click.pass_context
-def main(ctx: click.Context, debug: bool):
+def main(
+    ctx: click.Context,
+    debug: bool,
+    mqtt_host: str,
+    mqtt_username: str,
+    mqtt_password: str,
+):
+    logging.basicConfig(
+        level=logging.DEBUG if debug else logging.INFO,
+        format="%(asctime)s %(levelname)s %(module)s:%(lineno)d %(message)s",
+    )
+
     ctx.obj = Context(
         debug=debug,
         audio=Audio(),
+        mqtt=MQTT(
+            host=mqtt_host,
+            username=mqtt_username,
+            password=mqtt_password,
+        ),
     )
-
-    logging.basicConfig(level=logging.DEBUG if debug else logging.INFO)
 
 
 @main.command()
@@ -32,6 +51,15 @@ def list_audio_devices(ctx: click.Context):
     devices = ctx.obj.audio.list_input_devices()
     for idx, name in devices.items():
         click.echo(f"{idx}: {name}")
+
+
+@main.command()
+@click.pass_context
+def rms(ctx: click.Context):
+    ctx.obj.mqtt.connect()
+    for val in ctx.obj.audio.rms():
+        ctx.obj.mqtt.publish("rms", val)
+        log.debug("rms: %s", val)
 
 
 if __name__ == "__main__":
